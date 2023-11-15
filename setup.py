5f3e5e9ec9d721f4f19844a68aaf16ff5ad7c857
pdbang <pierre2bdn@gmail.com>
2023-11-15 11:32:54 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        "tabula-py"
+        "tabula-py",
+        "pandas",
+        "io",
+        "openai"
     ],
 )
