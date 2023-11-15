a4c6a144c5b4629ccb1975d638612afd9b2e34d1
pdbang <pierre2bdn@gmail.com>
2023-11-15 18:09:34 +0100

    pdf display

--- streamlit/app.py
+++ streamlit/app.py
@@ -11,12 +11,16 @@ def displayPDF(file):
     base64_pdf = base64.b64encode(file.getvalue()).decode('utf-8')
 
     # Embedding PDF in HTML
-    pdf_display =  f"""<embed class="pdfobject" type="application/pdf" title="Embedded PDF"
+    pdf_display =  f"""<embed class="pdfobject" 
+    type="application/pdf" 
+    title="Embedded PDF"
     src="data:application/pdf;base64,{base64_pdf}"
-    style="overflow: auto; width: 100%; height: 100%;">"""
+    style="overflow: auto; 
+    width: 100%; 
+    height: 100%;">"""
+    st.markdown(pdf_display, unsafe_allow_html=True)
     # pdf_display = f"""<object data="data:application/pdf;base64,{base64_pdf}" type="application/pdf" width="100%" height="100%"></object>"""
-    # pdf_display = f"""<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="100%"></iframe>"""
-    # Displaying File
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="100%" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
 
 if pdf_file:
